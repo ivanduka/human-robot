e38ec5f60d443799755e0e94825a43fd0f659d83
Ivan Duka <ivan.duka.pm@gmail.com>
2020-03-30 14:25:06 -0600

    extracting of images works

--- ext_funcs.py
+++ ext_funcs.py
@@ -1,24 +1,35 @@
-def extract_image(args):
-    table, engine, pdf_files_folder, jpg_tables_folder = args
-    table_id = table["tableId"]
-    pdf_name = table["pdfName"]
-    
-    
-    # try:
-    #     pdf_file_path = pdf_files_folder.joinpath(f"{table.fileId}.pdf")
+from wand.image import Image
+from sqlalchemy import text, create_engine
+from io import StringIO
+import sys
+from contextlib import redirect_stdout, redirect_stderr
+from pathlib import Path
 
-    #     with Image(filename=f"{pdf_file_path.resolve()}[{table.page - 1}]", resolution=600) as img:
-    #         left = int(table.pdfX1 * img.width / table.pdfWidth)
-    #         top = int((table.pdfHeight - table.pdfY1) * img.height / table.pdfHeight)
-    #         right = int(table.pdfX2 * img.width / table.pdfWidth)
-    #         bottom = int((table.pdfHeight - table.pdfY2) * img.height / table.pdfHeight)
-    #         img.crop(left=left, top=top, right=right, bottom=bottom)
-    #         img.format = "jpg"
-    #         img.save(filename=jpg_tables_folder_path.joinpath(f"{table.uuid}.jpg"))
-    #         print(f"Extracted table ID {table.uuid} to image")
-    # except Exception as e:
-    #     print(f"==== Error extracting table ID {table.uuid}  ======")
-    #     print(e)
-    #     print(f"======================================")
-    #     return
-    
+
+def extract_image(args):
+    buf = StringIO()
+    with redirect_stdout(buf), redirect_stderr(buf):
+        table, engine_string, pdf_files_folder_string, jpg_tables_folder_string = args
+        pdf_files_folder = Path(pdf_files_folder_string)
+        jpg_tables_folder = Path(jpg_tables_folder_string)
+        engine = create_engine(engine_string)
+            
+        try:
+            pdf_file_path = pdf_files_folder.joinpath(f'{table["pdfName"]}.pdf')
+            img_arg_string = f'{pdf_file_path.resolve()}[{table["page"] - 1}]'
+            with Image(filename=img_arg_string, resolution=300) as img:
+                left = round(table["pdfX1"] * img.width / table["pdfWidth"])
+                top = round((table["pdfHeight"] - table["pdfY1"]) * img.height / table["pdfHeight"])
+                right = round(table["pdfX2"] * img.width / table["pdfWidth"])
+                bottom = round((table["pdfHeight"] - table["pdfY2"]) * img.height / table["pdfHeight"])
+                img.crop(left=left, top=top, right=right, bottom=bottom)
+                img.format = "jpg"
+                img.save(filename=jpg_tables_folder.joinpath(f'{table["tableId"]}.jpg'))
+                with engine.connect() as conn:
+                    statement = text("UPDATE tables SET imageExtracted = 'done' WHERE tableId = :tableId;")
+                    result = conn.execute(statement, {"tableId": table["tableId"]})
+                print(f'Successfully inserted {result.rowcount} rows for {table["tableId"]}')
+        except Exception as e:
+            print(f'Error extracting {table["tableId"]}: {e}')
+        finally:
+            return buf.getvalue()
