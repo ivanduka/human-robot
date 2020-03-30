e7d387e9a4c73419b1eecb8fce87e04930821bc5
Ivan Duka <ivan.duka.pm@gmail.com>
2020-03-30 14:35:30 -0600

    autoformat

--- ext_funcs.py
+++ ext_funcs.py
@@ -13,22 +13,29 @@ def extract_image(args):
         pdf_files_folder = Path(pdf_files_folder_string)
         jpg_tables_folder = Path(jpg_tables_folder_string)
         engine = create_engine(engine_string)
-            
+
         try:
-            pdf_file_path = pdf_files_folder.joinpath(f'{table["pdfName"]}.pdf')
+            pdf_file_path = pdf_files_folder.joinpath(
+                f'{table["pdfName"]}.pdf')
             img_arg_string = f'{pdf_file_path.resolve()}[{table["page"] - 1}]'
             with Image(filename=img_arg_string, resolution=300) as img:
                 left = round(table["pdfX1"] * img.width / table["pdfWidth"])
-                top = round((table["pdfHeight"] - table["pdfY1"]) * img.height / table["pdfHeight"])
+                top = round((table["pdfHeight"] - table["pdfY1"])
+                            * img.height / table["pdfHeight"])
                 right = round(table["pdfX2"] * img.width / table["pdfWidth"])
-                bottom = round((table["pdfHeight"] - table["pdfY2"]) * img.height / table["pdfHeight"])
+                bottom = round(
+                    (table["pdfHeight"] - table["pdfY2"]) * img.height / table["pdfHeight"])
                 img.crop(left=left, top=top, right=right, bottom=bottom)
                 img.format = "jpg"
-                img.save(filename=jpg_tables_folder.joinpath(f'{table["tableId"]}.jpg'))
+                img.save(filename=jpg_tables_folder.joinpath(
+                    f'{table["tableId"]}.jpg'))
                 with engine.connect() as conn:
-                    statement = text("UPDATE tables SET imageExtracted = 'done' WHERE tableId = :tableId;")
-                    result = conn.execute(statement, {"tableId": table["tableId"]})
-                print(f'Successfully inserted {result.rowcount} rows for {table["tableId"]}')
+                    statement = text(
+                        "UPDATE tables SET imageExtracted = 'done' WHERE tableId = :tableId;")
+                    result = conn.execute(
+                        statement, {"tableId": table["tableId"]})
+                print(
+                    f'Successfully inserted {result.rowcount} rows for {table["tableId"]}')
         except Exception as e:
             print(f'Error extracting {table["tableId"]}: {e}')
         finally:
