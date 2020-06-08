f3c23ca5a441970e17122bb43431d553f5c4b1b4
Ivan Duka <ivan.duka.pm@gmail.com>
2020-06-08 14:55:31 -0600

    unregistering service worker

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { pdfjs } from "react-pdf";
+import { unregister } from './serviceWorker';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -11,4 +12,4 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+unregister();
